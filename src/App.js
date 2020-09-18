ca225b9cc34c89994811cd9d17292ddc86d9d2c0
dhyanasloka <dhyanasloka@gmail.com>
2020-09-18 12:57:35 +0200

    cookie bot

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GlobalStyle from './globalStyles';
+import CookieBot from 'react-cookiebot/lib/CookieBot';
 import Home from './pages/HomePage/Home';
 import Videos from './pages/VideosPage/Videos';
 import Articles from './pages/ArticlesPage/Articles';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop';
 import { Navbar, Footer } from './components';
+// import { Button, View, Text } from 'react-native';
+
+const domainGroupId = 'f096c79d-2c79-435a-8636-685723d03031';
+
 
 function App() {
+  const [hasCookieBot, setHasCookieBot] = useState(undefined);
   return (
+
     <Router basename={process.env.PUBLIC_URL}>
       <GlobalStyle />
       <ScrollToTop />
@@ -19,6 +26,15 @@ function App() {
         <Route path="/articles" component={Articles} />
       </Switch>
       <Footer />
+      {/* <div style={{ flex: 1, alignItems: 'center' }}>
+        <CookieBot domainGroupId={domainGroupId} />
+        <p>Click to test Cookiebot</p>
+        <button title="TEST" onPress={() => setHasCookieBot(!!document.querySelector('#CookieBot'))} >
+          TEST</button>
+        <div style={{ color: 'red', marginVertical: 10 }}>
+          {hasCookieBot && `Has CookieBot: ${JSON.stringify(hasCookieBot)}`}
+        </div>
+      </div> */}
     </Router>
   )
 }
